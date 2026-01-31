894f9f3f61e97ead4c54baf11e23a19d39427fe9
KaliMerO666 <kalimero@ik.me>
2026-02-01 00:34:27 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-3xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-8xl whitespace-nowrap bg-clip-text ">
-				www.kalimero.rocks
+				www.kalimero.ink
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
