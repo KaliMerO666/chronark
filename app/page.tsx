dbfb5e82868b242594c110fad34073372775722b
KaliMerO666 <kalimero@ik.me>
2026-02-02 05:34:13 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const navigation = [
 	{ name: "Site", href: "https://station-service.vercel.app" },
 	{ name: "About", href: "https://yamwhatyam.vercel.app" },
 	{ name: "Gallery", href: "https://picabooo.netlify.app" },
-{ name: "Publications", href: "https://nestinpace.netlify.app" },
+{ name: "Publications", href: "https://nestinpace.vercel.app" },
 	{ name: "Shop", href: "https://ko-fi.com/kalimero_666/shop" },
 	{ name: "Contact", href: "/contact" },
 ];
