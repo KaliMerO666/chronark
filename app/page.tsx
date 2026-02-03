ea1c1425033ed86055f8ccfe3066b91c5f8ff934
KaliMerO666 <kalimero@ik.me>
2026-02-03 10:57:48 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ const navigation = [
 	{ name: "About", href: "https://yamwhatyam.vercel.app" },
 	{ name: "Gallery", href: "https://picabooo.netlify.app" },
 { name: "Art Majeur", href: "https://www.artmajeur.com/olivier-kalimero-quemere" },
-
 { name: "Works", href: "https://nestinpace.vercel.app" },
 	{ name: "Shop", href: "https://ko-fi.com/kalimero_666/shop" },
 	{ name: "Contact", href: "/contact" },
