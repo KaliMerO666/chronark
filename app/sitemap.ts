0b88a6ebf7a856caf6a6237452a34b76bbd2b556
KaliMerO666 <kalimero@ik.me>
2026-02-27 13:58:55 +0100

    Add files via upload

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://kalimero.ink/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
